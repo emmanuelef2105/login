eafa933db977f2acb76dabc902e682b83bfc046a
Emmanuel Escobar Fernandeza <emmanuel.escobar@Quipux.local>
2024-12-30 14:23:14 -0500

    actaulización

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,7 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+
 
 <template>
   <div>
-    <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-
-        <nav>
-          <RouterLink to="/" exact>Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-          <RouterLink to="/admin">Admin</RouterLink>
-          <RouterLink to="/user">User</RouterLink>
-        </nav>
-      </div>
-    </header>
-
     <div>
       <RouterView />
     </div>
